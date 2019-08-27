b29542b328ded87d382885ff0da9c53aec2fe1bf
Dahyun Kim <beska@naver.com>
2019-08-27 23:14:14 +0800

    Updates

---
+++ precache-manifest.d18856a1f109b34f6b6f5c7093f4814b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57285799f17cb63501984e95c344fa8c",
+    "url": "/material-matters/index.html"
+  },
+  {
+    "revision": "e1c1eb8c841475d759b4",
+    "url": "/material-matters/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "8e41fea3378dea6c9072",
+    "url": "/material-matters/static/css/main.512d83f9.chunk.css"
+  },
+  {
+    "revision": "e1c1eb8c841475d759b4",
+    "url": "/material-matters/static/js/2.59c061b9.chunk.js"
+  },
+  {
+    "revision": "8e41fea3378dea6c9072",
+    "url": "/material-matters/static/js/main.9e2d0891.chunk.js"
+  },
+  {
+    "revision": "0c7a19fae65b7bde62f7",
+    "url": "/material-matters/static/js/runtime~main.f89498b8.js"
+  }
+]);
