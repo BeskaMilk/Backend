b483b427c95a4e7adea97e6d84a9ebb41242c6b4
Dahyun Kim <beska@naver.com>
2019-08-28 17:36:38 +0800

    server host changed
    api

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const config = require('universal-config');
 const Unsplash = require('unsplash-js').default;
 const toJson = require('unsplash-js').toJson;
 const express = require('express');
+
+const request = require('request');
 // const cors = require('cors')
 
 
@@ -14,27 +16,37 @@ const unsplash =new Unsplash({
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
+
+  // added
+app.get('/api/photos', (req, res) => {
+    request(
+      { url: 'https://api.unsplash.com/api/photos' },
+      (error, response, body) => {
+        if (error || response.statusCode !== 200) {
+          return res.status(500).json({ type: 'error', message: err.message });
+        }
+  
+        res.json(JSON.parse(body));
+      }
+    )
+  });
+
 //use this to use Unslash photos
-app.get('/api/photos', (req, res) => { 
-    //  res.setHeader('Access-Control-Allow-Origin', 'https://materialmatters.herokuapp.com/');
-    //  res.setHeader('Access-Control-Allow-Origin', 'https://beskamilk.github.io/');
-    
-    unsplash.photos
-        .listPhotos(req.query.start, req.query.count)
-        .then(toJson)
-        .then(json => res.json(json));
-});
+// app.get('/api/photos', (req, res) => {    
+//     unsplash.photos
+//         .listPhotos(req.query.start, req.query.count)
+//         .then(toJson)
+//         .then(json => res.json(json));
+// });
 
-//const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-// app.use(function (req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-//     next();
-//   });
+//const BACKEND = process.env.CALLBACK_URL || 'http://beskamilk.github.io/backend/' //'Access-Control-Allow-Origin: https://materialmatters.herokuapp.com/'
 
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-const BACKEND = process.env.CALLBACK_URL || 'http://beskamilk.github.io/backend/' //'Access-Control-Allow-Origin: https://materialmatters.herokuapp.com/'
-
-//app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-app.listen(BACKEND, () => console.log(`Server started on port ${BACKEND}`));
+//app.listen(BACKEND, () => console.log(`Server started on port ${BACKEND}`));
